23fc8a1afba4c3b69c6129491f0c7bdce58181e3
Yuli Mendez <yulianamendez1994@gmail.com>
2024-10-15 17:33:34 -0300

    Agrego la funcion cargar datos, terminada la parte de carga de datos

--- js/main.js
+++ js/main.js
@@ -3,4 +3,17 @@ function calcular_puntos (partidosg,partidosp,partidose) {
     return puntos
 }
 
+function cargardatos (numero_equipo) {
+    let ganados = parseInt(prompt("Inrgese partidos ganados del equipo " + numero_equipo))
+    let perdidos = parseInt(prompt("Inrgese partidos perdidos del equipo " + numero_equipo))
+    let empatados = parseInt(prompt("Inrgese partidos empatados del equipo " + numero_equipo))
+    let puntos = calcular_puntos(ganados, perdidos, empatados)
+    return puntos
+}
+
 let cantidadequipos = parseInt(prompt("Ingrese cantidad de equipos"))
+lista_puntos = []
+for (let i = 1; i <= cantidadequipos; i++ ) {
+    const puntos_equipo = cargardatos(i)
+    lista_puntos.push(puntos_equipo)
+}
