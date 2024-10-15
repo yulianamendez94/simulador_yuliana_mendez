48b417802ada5f534f2daaf4895d080e5bfaf2d0
Yuli Mendez <yulianamendez1994@gmail.com>
2024-10-15 17:29:45 -0300

    Agrego funcion para calcular puntos, todavia no la uso

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function calcular_puntos (partidosg,partidosp,partidose) {
+    let puntos = partidosg*3 + partidosp*0 + partidose*1
+    return puntos
+}
+
+let cantidadequipos = parseInt(prompt("Ingrese cantidad de equipos"))
